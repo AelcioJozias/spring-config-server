62aa961aee7916c6f1024a7cf9fb8621025cf0dd
Aelcio Jozias <aelciojoziasp@gmail.com>
2025-07-22 19:06:24 -0300

    Initial commit

---
+++ src/test/java/br/com/jozias/springconfigserver/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jozias.springconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
